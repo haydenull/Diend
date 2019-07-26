dab7e8c9aaa62d73ee3ce94fb8fb604f395b3491
Haaayden <brooklyn1984@qq.com>
2019-07-26 16:20:07 +0800

    feat(root): initial commit 配置 ts 开发环境

---
+++ src/platforms/web/entry-runtime-with-compiler.ts
@@ -0,0 +1,9 @@
+const Diend = {
+  diend: 'kamen rider diend',
+}
+
+function add() {
+  console.log('function add')
+}
+
+export default Diend
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/platforms/web/entry-runtime-with-compiler.ts',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'ded.min.js',
+    libraryTarget: 'umd',
+    library: 'Diend',
+    libraryExport: 'default',
+  },
+  resolve: {
+    extensions: [ '.ts' ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+}
